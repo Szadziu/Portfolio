b36dd3aea8900ebd3294a0069291dcbed21227b6
Szadziu <mcj.mikolajczak@gmail.com>
2021-12-27 22:34:22 +0100

    added new statements for a responsive page

--- src/constants/devices.js
+++ src/constants/devices.js
@@ -1,20 +1,25 @@
 const size = {
-  mobileS: "320px",
-  mobileM: "375px",
-  mobileL: "425px",
-  tablet: "768px",
-  laptop: "1024px",
-  laptopL: "1440px",
-  desktop: "2560px",
+  // urządzenia mobilne
+  mobileS: "320px", // 320x568 - iPhone 5
+  mobileM: "375px", // 375x667 iPhone 6/7/8
+  mobileL: "414px", // 414x736 iPhone 6/7/8 Plus
+  tabletS: "640px", // 640x480 small Tablet
+  tabletM: "768px", // 768x1024 iPad
+  tabletL: "1024px", //1024x1366 iPad Pro
+  // urządzenia desktopowe
+  laptop13: "1280px", // 1280x720
+  laptop15: "1366px", // 1366x768
+  desktopHD: "1920px", // 1920x1080
 };
 
 export const device = {
   mobileS: `(min-width: ${size.mobileS})`,
   mobileM: `(min-width: ${size.mobileM})`,
   mobileL: `(min-width: ${size.mobileL})`,
-  tablet: `(min-width: ${size.tablet})`,
-  laptop: `(min-width: ${size.laptop})`,
-  laptopL: `(min-width: ${size.laptopL})`,
-  desktop: `(min-width: ${size.desktop})`,
-  desktopL: `(min-width: ${size.desktop})`,
+  tabletS: `(min-width: ${size.tabletS})`,
+  tabletM: `(min-width: ${size.tabletM})`,
+  tabletL: `(min-width: ${size.tabletL})`,
+  laptop13: `(min-width: ${size.laptop13})`,
+  laptop15: `(min-width: ${size.laptop15})`,
+  desktopHD: `(min-width: ${size.desktopHD})`,
 };
--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -30,9 +30,81 @@ const GlobalStyle = createGlobalStyle`
         }
     }
 
+    @media ${device.desktopHD} {
+        body{
+            background-position: -45vw -20vh;
+            background-size: 90%;
+        }
+    }
+
+    @media ${device.laptop15} {
+        body{
+            background-position: -50vw -30vh;
+            background-size: 100%;
+        }
+    }
+
+    @media ${device.laptop13} {
+        body{
+            background-position: -40vw -15vh;
+            background-size: 80%;
+        }
+    }
+
+    @media ${device.tabletL} {
+        body{
+            background-position: -60vw 20vh;
+            background-size: 120%;
+        }
+    }
+
+    @media ${device.tabletL} and (orientation: landscape){
+        body{
+            background-position: -65vw -20vh;
+            background-size: 130%;
+        }
+    }
+
+    @media ${device.tabletM}{
+        body{
+            background-position: -75vw 0vh;
+            background-size: 150%;
+        }
+    }
+
+    @media ${device.tabletM} and (orientation: landscape){
+        body{
+            background-position: -60vw -20vh;
+            background-size: 120%;
+        }
+    }
+
+    @media ${device.tabletS}{
+        body{
+            background-position: -60vw -20vh;
+            background-size: 120%;
+        }
+    }
+
+
+    @media ${device.mobileL}{
+        body{
+            background-position: -80vw 20vh;
+            background-size: 160%;
+        }
+    }
+
+    @media ${device.mobileM}{
+        body{
+            background-position: -80vw 20vh;
+            background-size: 160%;
+        }
+    }
+
+
     @media ${device.mobileS}{
         body{
-            background-position: -75vw 40vh;
+            background-position: -75vw 30vh;
             background-size: 150%;
         }
     }
