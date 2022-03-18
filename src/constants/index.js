0c5023c35be47443070d05f66abc0d13aeb3efd1
Szadziu <mcj.mikolajczak@gmail.com>
2022-03-18 12:58:11 +0100

    clicker game project is live

--- src/constants/index.js
+++ src/constants/index.js
@@ -63,7 +63,6 @@ export const PROJECTS = [
     img: maptyIcon,
     link: 'https://szadziu.github.io/MaptyApp/',
     desc: 'Aplikacja służy do wyznaczania miejsc z użyciem rzeczywistej mapy, w których określamy typ treningu jaki odbyliśmy, przebyty dystans, czas jaki zajęło ćwiczenie oraz w zależności od typu treningu jedną z dodatkowych opcji. Aplikacja zapisuje lokalnie nasze treningi. Projekt przystosowany do urządzeń desktopowych. Technologie i biblioteki, które zostały użyte podczas tworzenia aplikacji: Javascript (z użyciem klas), leaflet (mapa), operacje z obiektem Date oraz localStorage',
-
   },
   {
     id: 2,
@@ -71,7 +70,6 @@ export const PROJECTS = [
     img: cssGeneratorIcon,
     link: 'https://szadziu.github.io/boxShadowCSSGenerator/',
     desc: 'Aplikacja służy do wygenerowania właściwości box-shadow (CSS), nadania jej własnych ustawień i zaprezentowania finalnego rezultatu. Istnieje również możliwość wybrania koloru przez użytkownika. Technologie i biblioteki, które zostały użyte podczas tworzenia aplikacji: React (komponenty funkcyjne), styled-components, react hooks',
-
   },
   {
     id: 3,
@@ -79,15 +77,13 @@ export const PROJECTS = [
     img: calendarIcon,
     link: 'https://szadziu.github.io/calendarApp/',
     desc: 'Kalendarz jest aplikacją umożliwiającą zapisywanie wydarzeń/zadań na poszczególne dni. Kalendarz jest rzeczywisty, wykorzystano również możliwość zapisu aktualnego stanu na serwerze z użyciem zapytań. Technologie i biblioteki, które zostały użyte podczas tworzenia aplikacji: React (komponenty funkcyjne), react-hooks, zapytania async/await, pre-processor Sass',
-
   },
   {
     id: 4,
     name: 'Clicker Browser Game',
     img: clickerGameIcon,
-    link: null,
+    link: 'https://centmsn.github.io/clicker-game',
     desc: 'Aplikacja przedstawiająca grę polegającą na klikaniu w skarb znajdujący się w głownym panelu użytkownika pozwala generować zasoby w postaci tzw. złota, monet. Z ich użyciem gracz ma za zadanie odblokowywać kolejnych bohaterów, którzy wspomogą go poprzez dostarczenie dodatkowych zasobów i umiejętności. Przyrost złota gracza rośnie wtedy w znaczącym tempie co pozwala na wykup coraz to potężniejszych bohaterów. Obecnie gra jest rozwijana, posiada już takie funkcje jak zapisywanie i resetowanie gry, przewidywana jest sekcja osiągnięć. Aplikacja ta od początku powstaje w zespole dwuosobowym. Technologie i biblioteki, które zostały użyte podczas tworzenia aplikacji: React (komponenty funkcyjne), styled-components, local Storage, react-hooks, react-redux',
-
   },
   {
     id: 5,
@@ -95,7 +91,6 @@ export const PROJECTS = [
     img: timerIcon,
     link: 'https://szadziu.github.io/stoper',
     desc: 'Aplikacja imitująca stoper, umożliwia odmierzanie, zatrzymywanie oraz resetowanie czasu. Technologie, które zostały zastosowane w projekcie to: React (functional components), styled-components, operacje z obiektem Math',
-
   },
   {
     id: 6,
@@ -103,7 +98,6 @@ export const PROJECTS = [
     img: shoppingListIcon,
     link: null,
     desc: 'Możliwości tej aplikacji, której zadaniem jest tworzenie list zakupowych są bardzo praktyczne. Użytkownik A może bowiem stworzyć listę, wysłać ją do innego użytkownika B, który obecnie robi zakupy. Wszyscy korzystający z danej listy mogą odznaczać lub dodawać nowe produkty, które od razu się zaaktualizują. Aplikacja pozwala również na oznaczanie już kupionuych produktów, lecz także przydzielenia statusu "obecnie brak" dzięki czemu użytkownik A może dany produkt zastąpić. Technologie i biblioteki, które zostały użyte podczas tworzenia aplikacji: React (komponenty funkcyjne), tailwind, react-query',
-
   },
   {
     id: 7,
@@ -111,7 +105,6 @@ export const PROJECTS = [
     img: sportsStoreIcon,
     link: null,
     desc: 'Projekt w budowie',
-
   },
   {
     id: 8,
@@ -119,7 +112,6 @@ export const PROJECTS = [
     img: recruitmentTaskIcon,
     link: null,
     desc: 'Tutaj pozostawiam miejsce na pierwsze zadanie rekrutacyjne :) Technologie, które będą zastosowane w projekcie: te najbardziej odpowiednie',
-
   },
 ];
 
