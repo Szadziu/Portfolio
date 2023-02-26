b5e3decbd4316c43585b34815e75d47b83fce872
Szadziu <mcj.mikolajczak@gmail.com>
2023-02-26 22:12:35 +0100

    changed styles of comment component

--- src/components/generics/Comment/comment.parts.js
+++ src/components/generics/Comment/comment.parts.js
@@ -6,9 +6,13 @@ const infoComment = css`
 `;
 
 export const Comment = styled.p`
+    position: absolute;
+    left: ${({ info }) => (info ? '20px' : "0")};
+    bottom: ${({ info }) => (info ? '30px' : "-20px")};
     width: 100%;
     height: 24px;
     line-height: 20px;
+    z-index: -1;
     padding-top: ${({ theme }) => theme.padding.s};
 
     color: ${({ error, theme }) => (error ? theme.pink_300 : theme.green_300)};
@@ -16,8 +20,8 @@ export const Comment = styled.p`
     ${({ info }) => (info ? infoComment : null)};
 
     font-size: ${({ theme }) => theme.fontSize.s};
-    font-weight: 300;
-
+    font-weight: ${({info}) => (info ? 600 : 300)};
+    
     @media ${device.mobileL} {
         font-size: ${({ theme }) => theme.fontSize.m};
     }
