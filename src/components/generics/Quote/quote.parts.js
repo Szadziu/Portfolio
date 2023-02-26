4d0ba6a84c7797ce2dd80646a625499fa4acc6a8
Szadziu <mcj.mikolajczak@gmail.com>
2023-02-26 22:13:00 +0100

    fix styles on quote

--- src/components/generics/Quote/quote.parts.js
+++ src/components/generics/Quote/quote.parts.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
-import { Flex } from '../../../styles/Mixins';
-import { device } from '../../../constants/devices';
+import {Flex} from '../../../styles/Mixins';
+import {device} from '../../../constants/devices';
 
 export const QuoteParam = styled.p`
     ${Flex()};
-    grid-area: 11/2/13/12;
+    grid-area: 12/2/13/12;
     opacity: 0;
-    padding: ${({ theme }) => theme.padding.xl};
+    padding: ${({theme}) => theme.padding.xl};
 
-    color: ${({ theme }) => theme.pink_300};
+    color: ${({theme}) => theme.pink_300};
 
     font-style: italic;
-    font-size: ${({ theme }) => theme.fontSize.m};
+    font-size: ${({theme}) => theme.fontSize.s};
 
     @media ${device.desktop} {
-        font-size: ${({ theme }) => theme.fontSize.l};
+        font-size: ${({theme}) => theme.fontSize.l};
     }
 `;
